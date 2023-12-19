c5888d84166e07bd63da9dae570b09d8b1c920e3
joaquincorimayo <joaquin97corimayo@gmail.com>
2023-12-18 22:41:50 -0300

    initial commit

---
+++ src/main/java/com/jfecm/springtransactions/SpringTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.jfecm.springtransactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfecm/springtransactions/SpringTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfecm.springtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
